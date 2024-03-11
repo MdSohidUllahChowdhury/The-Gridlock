251ff05a541454e91dce10270f7607da2a6ed965
Sohid Ullah <mdsohidullahchowdhury1@gmail.com>
2024-03-11 19:14:25 +0600

    Add

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,4 +1,5 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, sort_child_properties_last, non_constant_identifier_names
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, sort_child_properties_last, non_constant_identifier_names, prefer_interpolation_to_compose_strings
+
 import 'package:flutter/material.dart';
 
 class Screen extends StatefulWidget {
@@ -11,6 +12,8 @@ class _ScreenState extends State<Screen> {
 
 
   bool ohTurn = true; //! The frist player is "O"
+
+
   List<String> displayXO = [
     '',
     '',
@@ -80,20 +83,74 @@ class _ScreenState extends State<Screen> {
   }
 
   void tapted(int index) {
+    
     setState(() {
       if (ohTurn){
         displayXO[index] = 'O';
       }else{
         displayXO[index] = 'X';
       }
+
+      ohTurn = !ohTurn; //Ekbar ekta press krle next player er turn hobe.
+      CheckWinner();
     });
   }
 
-  void checkWinner(){
+  void CheckWinner(){
+//R1
+if(displayXO[0] == 
+   displayXO[1] && displayXO[0] == displayXO[2] && displayXO[0] != " ") {
+ShowWin(displayXO[0]);
+ }
 
-  }
+ //R2
+if(displayXO[3] == 
+   displayXO[4] && displayXO[3] == displayXO[5] && displayXO[3] != " ") {
+ShowWin(displayXO[3]);
+ }
 
-  void showWin(){
+ //R3
+if(displayXO[6] == 
+   displayXO[7] && displayXO[6] == displayXO[8] && displayXO[6] != " ") {
+ShowWin(displayXO[6]);
+ }
 
-  }
+//C1
+if(displayXO[0] == 
+   displayXO[3] && displayXO[0] == displayXO[6] && displayXO[0] != " ") {
+ShowWin(displayXO[0]);
+ }
+
+ //C2
+if(displayXO[1] == 
+   displayXO[4] && displayXO[1] == displayXO[7] && displayXO[1] != " ") {
+ShowWin(displayXO[1]);
+ }
+
+ //C3
+if(displayXO[2] == 
+   displayXO[5] && displayXO[2] == displayXO[8] && displayXO[2] != " ") {
+ShowWin(displayXO[2]);
+ }
+
+ //CROSS1
+if(displayXO[6] == 
+   displayXO[4] && displayXO[6] == displayXO[2] && displayXO[6] != " ") {
+ShowWin(displayXO[6]);
+ }
+
+ //CROSS2
+if(displayXO[0] == 
+   displayXO[4] && displayXO[0] == displayXO[8] && displayXO[0] != " ") {
+ShowWin(displayXO[0]);
+ }
+
 }
+
+  }
+
+  void ShowWin( String winner){
+
+  }
+
+    
